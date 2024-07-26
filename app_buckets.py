f4648c5693b5023f739acf47abce7486cac5cb0d
DaryPte <altamiranodary16@gmail.com>
2024-07-25 21:42:00 -0500

    feature: [DEV] prueba

--- app_buckets.py
+++ app_buckets.py
@@ -1,37 +1,60 @@
 # app_buckets.py
-from flask import Flask
-from flask import Flask, request, send_from_directory, abort
+from flask import Flask, request, send_from_directory, abort,jsonify
 import os
 app = Flask(__name__)
+import requests
 
 from google.cloud import storage
-from settings import GS_CREDENTIALS
+import settings
 
-def generate_signed_url(blob_name, expiration=3600):
+DOWNLOAD_DIR = r'descargas'
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
+
+def list_bucket_contents_and_generate_urls(prefix):
     storage_client = storage.Client(credentials=settings.GS_CREDENTIALS)
     bucket = storage_client.bucket(settings.GS_BUCKET_NAME)
-    blob = bucket.blob(blob_name)
 
-    url = blob.generate_signed_url(
-        version="v4",
-        expiration=datetime.timedelta(seconds=expiration),
-        method="GET",
-    )
-    return url
+    blobs = bucket.list_blobs(prefix=prefix, max_results=3000)
 
+    file_urls = []
+    for blob in blobs:
+        file_urls.append(blob.name)
 
-@app.route('/')
-def hello_world():
-    client = storage.Client(credentials=GS_CREDENTIALS)
-    bucket = client.get_bucket('bucket-sga')
-    # bucket = client.bucket('bucket-sga')
+    return file_urls
 
-    # link = bucket.blob(f'media/chayanne.png')
-    # link = generate_signed_url("media/Imagen/2022/05/03/estadohelpdesk_20225316361.png")
-    link = generate_signed_url("media/03.jpg")
+def download_file_from_api(file_name):
+    url = f"http://34.86.255.76:5000/get-file?path={file_name}"
+    print(f"Downloading {file_name} from {url}")
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_path = os.path.join(DOWNLOAD_DIR, file_name)
+        # Crear directorios si no existen
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded {file_name} to {file_path}")  # Mensaje de depuración
+        return file_path
+    else:
+        print(f"Failed to download {file_name}, status code: {response.status_code}")  # Mensaje de depuración
+        raise Exception(f"Failed to download file: {file_name}, status code: {response.status_code}")
 
-    return f"Archivos en el bucket: {link}"
-
+@app.route('/list-files')
+def list_files():
+    try:
+        prefix = 'media/AgendamientoCita/'
+        file_urls = list_bucket_contents_and_generate_urls(prefix)
+        downloaded_files = []
+        errors = []
+        for file_url in file_urls:
+            try:
+                downloaded_file = download_file_from_api(file_url)
+                downloaded_files.append(downloaded_file)
+            except Exception as e:
+                errors.append({"file": file_url, "error": str(e)})
+        return jsonify({"downloaded_files": downloaded_files, "errors": errors})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 BASE_DIR = '/home/daltamirano/bucket-sga'
@@ -56,5 +79,5 @@ def get_file():
     return send_from_directory(BASE_DIR, file_path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
